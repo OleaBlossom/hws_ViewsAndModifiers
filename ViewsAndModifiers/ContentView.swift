34fc7d559c205f47a168bc783edf07dc562479a9
olea <olea.plum@gmail.com>
2022-07-12 10:27:58 +0200

    create custom view modifier and extension for adding a watermark

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,14 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		VStack(spacing: 10) {
-			Text("Hello World")
-				.titleStyle()
-			CapsuleText(text: "First")
-				.foregroundColor(.white)
-			CapsuleText(text: "Second")
-				.foregroundColor(.primary)
-		}
+		Color.blue
+			.frame(width: 300, height: 200)
+			.watermarked(with: "Hacking with Swift")
 	}
 }
 
@@ -61,5 +56,26 @@ struct Title: ViewModifier {
 extension View {
 	func titleStyle() -> some View {
 		modifier(Title())
+	}
+}
+
+struct Watermark: ViewModifier {
+	var text: String
+	
+	func body(content: Content) -> some View {
+		ZStack(alignment: .bottomTrailing) {
+			content
+			Text(text)
+				.font(.caption)
+				.foregroundColor(.white)
+				.padding(5)
+				.background(.black)
+		}
+	}
+}
+
+extension View {
+	func watermarked(with text: String) -> some View {
+		modifier(Watermark(text: text))
 	}
 }
